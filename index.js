556a680109dd97b8fa7014ce44771815479fc66c
Ramiro <ramifazio@gmail.com>
2022-09-13 19:49:23 -0300

    Sugerencias!

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-let edad = prompt("Bienvenido a nuestro Casino Online, por favor ingrese su edad")
+let edad = parseInt(
+  prompt("Bienvenido a nuestro Casino Online, por favor ingrese su edad"),
+);
 
-while (edad !="ESC") {
-    if (parseInt(edad) <17) {
-        alert("No puedes entrar a este sitio.");
-    } else if (parseInt(edad) < 18) {
-        alert("Puedes entrar con un adulto, pero no puedes realizar apuestas.");
-    } else {
-        alert("Bienvenido al Casino Online, mucha suerte en las apuestas!")
-    }
-
-    edad = prompt("Bienvenido a nuestro Casino Online, por favor ingrese su edad.")
+function getEdad(edad) {
+  //Funcion para obtener Edad
+  if (edad < 17) {
+    alert("No puedes entrar a este sitio.");
+  } else if (edad < 18) {
+    alert("Puedes entrar con un adulto, pero no puedes realizar apuestas.");
+  } else if (edad >= 18) {
+    //Si tiene mas de 18 pasa
+    alert("Bienvenido al Casino Online, mucha suerte en las apuestas!");
+  } else {
+    //VALIDAMOS que ponga algo si o si
+    alert("Por favor escribe tu edad");
+    edad = parseInt(
+      prompt("Bienvenido a nuestro Casino Online, por favor ingrese su edad"),
+    ); //Volvemos a pedir la edad
+    getEdad(edad); //Volvemos a llamar a la funcion getEdad con la nueva edad y vuelve a repetir el ciclo
+  }
 }
+
+getEdad(edad); //Llamamos a la funcion
+
+/*
+    Lo que use aca fue RECURSIVIDAD, no se ve en este curso, es una manera de volver a llamar una funcion dentro de otra, sirve para estos casos de validacion y muchisimas mas cosas, pero no te preocupes ;)
+*/
